5d77637263a9183000dc80016e3fcd3068244c2b
ac51300696 <783384903@qq.com>
2016-03-15 18:37:19 +0800

    2016年3月15日18:34:52
    2016年3月15日18:34:55

---
+++ text/text.js
@@ -0,0 +1,25 @@
+var text_app = require("../app"),
+    var boot = text_app.boot,
+        shotdown = text_app.shotdown,
+        port = text_app.port,
+        superagent = require("superagent"),
+        expect = require("expect.js");
+describe("server", function() {
+    before(function() {
+        boot();
+    });
+});
+describe("homepage", function() {
+    it("should respond to GET", function(done) {
+        superagent
+            .get("http://localhost:" + port)
+            .end(function(res) {
+                expect(res.status).to.equal(200);
+                done()
+            })
+    })
+});
+after(function() {
+shutdown()
+});
+});
