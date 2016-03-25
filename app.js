d81bd572da3301213971dff3ca0aa14ffb159de6
lxj <783384903@qq.com>
2016-03-25 17:28:54 +0800

    cookies session

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 var express = require('express'),
-  routes = require('./routes'),
-  http = require('http'),
-  path = require('path'),
-  mongoskin = require('mongoskin'),
-  dbUrl = process.env.MONGOHQ_URL || 'mongodb://@localhost:27017/blog',
-  db = mongoskin.db(dbUrl, {safe: true}),
-  collections = {
-    articles: db.collection('articles'),
-    users: db.collection('users')
-  };
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    mongoskin = require('mongoskin'),
+    dbUrl = process.env.MONGOHQ_URL || 'mongodb://@localhost:27017/blog',
+    db = mongoskin.db(dbUrl, { safe: true }),
+    collections = {
+        articles: db.collection('articles'),
+        users: db.collection('users')
+    };
 
 var session = require('express-session'),
-  logger = require('morgan'),
-  errorHandler = require('errorhandler'),
-  cookieParser = require('cookie-parser'),
-  bodyParser = require('body-parser'),
-  methodOverride = require('method-override');
+    logger = require('morgan'),
+    errorHandler = require('errorhandler'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 
 var app = express();
-app.locals.appTitle = 'blog-express';
+app.locals.appTitle = 'blog-lxj';
 
 app.use(function(req, res, next) {
-  if (!collections.articles || ! collections.users) return next(new Error("No collections."))
-  req.collections = collections;
-  return next();
+    if (!collections.articles || !collections.users) return next(new Error("No collections."))
+    req.collections = collections;
+    return next();
 });
 
 app.set('port', process.env.PORT || 3000);
@@ -33,13 +33,36 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(cookieParser("lxj-blog-cookie"));
+app.use(session({ secret: "lxj-blog-session" }))
 app.use(methodOverride());
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    if (req.session && req.session.admin) {
+        res.locals.admin = true;
+        console.log("session:ture");
+    }
+    if (req.cookies["login"] && req.cookies["login"] ==1) {
+        res.locals.admin = true;
+        console.log("cookie:ture");
+    }
+    next();
+})
+var authorize = function(req, res, next) {
+    if ((req.session && req.session.admin) || (req.cookies["login"] && req.cookies["login"] ==1)) {
+      console.log("admin")
+        return next();
+    } else {
+      console.log("not admin")
+        return res.send(401)
+    }
+}
+
 // development only
 if ('development' == app.get('env')) {
-  app.use(errorHandler());
+    app.use(errorHandler());
 }
 
 // Pages and routes
@@ -47,12 +70,13 @@ app.get('/', routes.index);
 app.get('/login', routes.user.login);
 app.post('/login', routes.user.authenticate);
 app.get('/logout', routes.user.logout);
-app.get('/admin',  routes.article.admin);
-app.get('/post',  routes.article.post);
-app.post('/post', routes.article.postArticle);
+app.get('/admin', authorize, routes.article.admin);
+app.get('/post', authorize, routes.article.post);
+app.post('/post', authorize, routes.article.postArticle);
 app.get('/articles/:slug', routes.article.show);
 
 // REST API routes
+app.all("/api", authorize)
 app.get('/api/articles', routes.article.list);
 app.post('/api/articles', routes.article.add);
 app.put('/api/articles/:id', routes.article.edit);
@@ -61,27 +85,27 @@ app.del('/api/articles/:id', routes.article.del);
 
 
 app.all('*', function(req, res) {
-  res.send(404);
+    res.send(404);
 })
 
 // http.createServer(app).listen(app.get('port'), function(){
-  // console.log('Express server listening on port ' + app.get('port'));
+// console.log('Express server listening on port ' + app.get('port'));
 // });
 
 var server = http.createServer(app);
-var boot = function () {
-  server.listen(app.get('port'), function(){
-    console.info('Express server listening on port ' + app.get('port'));
-  });
+var boot = function() {
+    server.listen(app.get('port'), function() {
+        console.info('Express server listening on port ' + app.get('port'));
+    });
 }
 var shutdown = function() {
-  server.close();
+    server.close();
 }
 if (require.main === module) {
-  boot();
+    boot();
 } else {
-  console.info('Running app as a module')
-  exports.boot = boot;
-  exports.shutdown = shutdown;
-  exports.port = app.get('port');
+    console.info('Running app as a module')
+    exports.boot = boot;
+    exports.shutdown = shutdown;
+    exports.port = app.get('port');
 }
